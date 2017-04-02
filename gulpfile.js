55d8840824c6aba421cb78edd13e3991674f99ab
Samuel Felício Adriano <felicio.samuel@gmail.com>
2017-04-02 15:06:35 -0300

    Adjust gulpfile to generate min only to doneto.css

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,27 @@
-var gulp         = require('gulp'),
-    sass         = require('gulp-ruby-sass'),
-    cssnano      = require('gulp-cssnano'),
-    rename       = require('gulp-rename'),
-    notify       = require('gulp-notify');
+const CSS_FILE = 'doneto.css',
+      MIN_FILE = 'doneto.min.css',
+      PATH     = {
+          scss : './scss',
+          css  : './css',
+          min  : './css'
+};
+
+var gulp    = require('gulp'),
+    sass    = require('gulp-ruby-sass'),
+    cssnano = require('gulp-cssnano'),
+    rename  = require('gulp-rename'),
+    notify  = require('gulp-notify');
 
 gulp.task('css', function() {
-  return sass('./scss/*.scss', { style: 'expanded' })
-    .pipe(gulp.dest('./css'))
+  return sass(PATH.scss + '/*.scss', { style: 'expanded' })
+    .pipe(gulp.dest(PATH.css))
     .pipe(notify({message: 'CSS generated.'}));
 });
 
 gulp.task('minify', function() {
-    return gulp.src('./css/*.css')
+    return gulp.src(PATH.css + '/' + CSS_FILE)
         .pipe(rename({suffix: '.min'}))
         .pipe(cssnano())
-        .pipe(gulp.dest('./css'))
+        .pipe(gulp.dest(PATH.min))
         .pipe(notify({message: 'Minified CSS generated.'}));
 });
